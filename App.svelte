7e87fb61d1524131f1d37fcb42b3f5bb1e13ed9d
Jay Wang <jay@zijie.wang>
2023-04-24 02:39:19 -0400

    Init commit
    Signed-off-by: Jay Wang <jay@zijie.wang>

---
+++ App.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  import Supernova from './src/components/supernova/SuperNOVA.svelte';
+</script>
+
+<style></style>
+
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <link
+    href="https://fonts.googleapis.com/css2?family=Lato&display=swap"
+    rel="stylesheet"
+  />
+</svelte:head>
+
+<div class="stand-alone-page">
+  <Supernova />
+</div>
---
+++ src/components/supernova/SuperNOVA.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+  import logoDiffusionDB from '../../imgs/logo-diffusiondb.svg?raw';
+  import iconGithub from '../../imgs/icon-github.svg?raw';
+
+  let component: HTMLElement | null = null;
+</script>
+
+<style lang="scss">
+  @import './supernova.scss';
+</style>
+
+<div class="supernova-page">
+  <div id="popper-tooltip-top" class="popper-tooltip hidden" role="tooltip">
+    <span class="popper-content" />
+    <div class="popper-arrow" />
+  </div>
+
+  <div id="popper-tooltip-bottom" class="popper-tooltip hidden" role="tooltip">
+    <span class="popper-content" />
+    <div class="popper-arrow" />
+  </div>
+
+  <div class="app-wrapper">
+    <div class="main-app" bind:this={component}>
+      <div class="main-app-container">Hello</div>
+    </div>
+  </div>
+
+  <div class="footer-container">Footer</div>
+
+  <div class="search-panel-container">Search</div>
+</div>
